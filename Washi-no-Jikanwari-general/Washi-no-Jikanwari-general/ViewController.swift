088b4adb2dc0cd64b9ca8650a28c6d42a25105f2
Kawai Ryusei <Chell@infonomacbook-air-50.local>
2021-10-14 15:14:09 +0900

    test

---
+++ Washi-no-Jikanwari-general/Washi-no-Jikanwari-general/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Washi-no-Jikanwari-general
+//
+//  Created by Kawai Ryusei on 2021/10/14.
+//  Copyright © 2021 Kawai Ryusei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
